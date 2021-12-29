3ebed7363a086bb8b274a31365ab0ed6c387f1ff
kingcode <samuelvilorio_15@hotmail.com>
2021-12-28 23:51:10 -0400

    build: setting league-model

---
+++ models/League.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const leagueSchema = mongoose.Schema({
+    name: {
+        type: String,
+        min: 3,
+        required: true
+    },
+    founder: {
+        type: [String],
+        required: true
+    },
+    manager: {
+        type: [String] //string de ids o hacer un array con un modelo de un admin?
+    },
+    foundingDate: {
+        type: Date,
+        required: true
+    },
+    phone: {
+        type: [String],
+        required: true
+    },
+    colors:{
+        type: [String],
+        required: true
+    },
+    location: {
+        type: String,
+        min: 6,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+});
+module.exports = mongoose.model('League', leagueSchema);
