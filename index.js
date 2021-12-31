cc3952af2762b8e9f7db9a270646bd6d79fa55e3
kingcode <samuelvilorio_15@hotmail.com>
2021-12-30 23:32:36 -0400

    build: league-route

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const leagueRouter = require('./routes/league');
 
+//Middleware for responses
+app.use(express.json());
+
+//Route middleware
+app.use('/league', leagueRouter);
 
 //Setting the port
 app.listen(3000, () => console.log('Valen3 is up!'));
