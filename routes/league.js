7a3bd1a3ecde2d932030eb7a2795b7daf143a2bb
kingcode <samuelvilorio_15@hotmail.com>
2022-01-06 23:47:35 -0400

    Feat: register a league

--- routes/league.js
+++ routes/league.js
@@ -6,8 +6,22 @@ const League = require('../models/League');
         res.send('This is your League!');
     });
     //Register league
-    router.post('/create', (req, res) =>{
-        res.send('This is the create route');
+    router.post('/create', async (req, res) =>{
+        const league = new League({
+            name: req.body.name,
+            founder: [req.body.founder],
+            manager: [req.body.manager],
+            foundingDate: req.body.foundingDate,
+            phone: req.body.phone,
+            colors: [req.body.colors],
+            location: req.body.location,
+            email: req.body.email
+        });
+        try {
+            const savedLeague = await league.save();
+        } catch (error) {
+            res.json({message: error});
+        }
     });
     //Update league info
     router.get('/update', (req, res) =>{
