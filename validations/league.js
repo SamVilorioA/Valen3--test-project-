bbdf33d613004fe8a46faf2ff8e43975b0dff953
kingcode <samuelvilorio_15@hotmail.com>
2022-01-10 23:27:49 -0400

    build: finishing the set up of the validation file

--- validations/league.js
+++ validations/league.js
@@ -3,8 +3,12 @@ const Joi = require('joi');
 const validation = (data) =>{
     const schema = Joi.object({
         name: Joi.string().min(6).required(),
-        location: Joi.string().min(5).required(),
+        founder: Joi.array().items(Joi.string().required()),
+        manager: Joi.array().items(Joi.string().required()),
         foundingDate: Joi.date().required(),
+        phone: Joi.array().items(Joi.string().min(4).required()),
+        colors: Joi.array().items(Joi.string().required(), Joi.string().required()),
+        location: Joi.string().min(5).required(),
         email: Joi.string().required().email()
     });
     return schema.validate(data);
